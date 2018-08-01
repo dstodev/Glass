3f49f9fb686380e401d731cb1a6f8716b0b3e3d5
dstodev <dstodev@gmail.com>
2018-07-31 21:21:30 -0700

    Extra comments

--- modules/mutator.py
+++ modules/mutator.py
@@ -7,20 +7,25 @@ client = Glass()
 
 @client.decorate_delegate("on_message")
 def permit(ctx: Delegate):
+    # Create fire closure
     fire = ctx.fire
+
+    # Create permission objects
     if ctx.permission is None:
         permission = discord.Permissions.none()
     else:
         permission = ctx.permission
 
+    # Create wrapped function with permissions
     async def permitted_event(message: discord.Message):
         channel = message.channel  # type: discord.Channel
         author = message.author  # type: discord.User
 
         # Apply permissions
-        if permission and author.permissions_in(channel) >= permission:
+        if author.permissions_in(channel) >= permission:
             await fire(message)
 
+    # Overwrite handler with one with permissions
     ctx.fire = permitted_event
 
     return ctx
